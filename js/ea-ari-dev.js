9d2de28289fc677020bd2957cf1e4eedce870220
Les agents <nrauber@les-agents-intelligents.com>
2025-02-27 12:47:22 +0100

    Log the current page URL

---
+++ js/ea-ari-dev.js
@@ -0,0 +1,50 @@
+//ea-ari.js
+const CustomOpenURLExtension = {
+    name: "CustomOpenURLExtension",
+    type: "effect",
+
+    match: ({ trace }) => {
+        return trace.type === "CustomOpenURLExtension";
+    },
+
+    effect: ({ trace }) => {
+        // Ensure the payload contains a valid URL
+        if (!trace.payload || !trace.payload.url) {
+            console.warn("No URL found in trace payload.");
+            return;
+        }
+
+        const url = trace.payload.url.trim();
+
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            console.warn("Invalid URL:", url);
+            return;
+        }
+        // Redirect the page to the given URL
+        window.location.href = url;
+    }
+};
+
+// Log the current page URL
+console.log("Current page URL:", window.location.href);
+
+(function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+        window.voiceflow.chat.load({
+            verify: { projectID: '679bba21dbd2e6376007c5b8' },
+            url: 'https://general-runtime.voiceflow.com',
+            versionID: 'production', 
+            assistant: { 
+                extensions: [CustomOpenURLExtension],
+                stylesheet:'https://ea-ari-demo1.vercel.app/voiceflow.fr.css'
+             },
+            voice: { 
+                url: "https://runtime-api.voiceflow.com" 
+            }
+        });
+    };
+    v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+    v.type = "text/javascript";
+    s.parentNode.insertBefore(v, s);
+})(document, 'script');
