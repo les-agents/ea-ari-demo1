eb1e5f78a3e5d7b9f0d847f4fda84add6a3ef53d
les-agents <nrauber@les-agents-intelligents.com>
2025-03-04 11:58:48 +0100

    nouvelle version

--- js/ea-ari.js
+++ js/ea-ari.js
@@ -29,7 +29,7 @@ const CustomOpenURLExtension = {
     var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
     v.onload = function() {
         window.voiceflow.chat.load({
-            verify: { projectID: '679bba21dbd2e6376007c5b8' },
+            verify: { projectID: '67c42da6a9ca2ac532c2c721' },
             url: 'https://general-runtime.voiceflow.com',
             versionID: 'production', 
             assistant: { 
