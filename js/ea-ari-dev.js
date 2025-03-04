f10a2737b5e65d95838e774412419170106a7b5d
les-agents <nrauber@les-agents-intelligents.com>
2025-03-04 16:11:49 +0100

    without ext

--- js/ea-ari-dev.js
+++ js/ea-ari-dev.js
@@ -1,29 +1,3 @@
-//ea-ari.js
-const CustomOpenURLExtension = {
-    name: "CustomOpenURLExtension",
-    type: "effect",
-
-    match: ({ trace }) => {
-        return trace.type === "CustomOpenURLExtension";
-    },
-
-    effect: ({ trace }) => {
-        // Ensure the payload contains a valid URL
-        if (!trace.payload || !trace.payload.url) {
-            console.warn("No URL found in trace payload.");
-            return;
-        }
-
-        const url = trace.payload.url.trim();
-
-        if (!url.startsWith("http://") && !url.startsWith("https://")) {
-            console.warn("Invalid URL:", url);
-            return;
-        }
-        // Redirect the page to the given URL
-        window.location.href = url;
-    }
-};
 
 
   (function(d, t) {
