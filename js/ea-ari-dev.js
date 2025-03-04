52064573399b5070ddbd7a0c9792eb4545322019
les-agents <nrauber@les-agents-intelligents.com>
2025-03-04 15:48:05 +0100

    re-update to legacy

--- js/ea-ari-dev.js
+++ js/ea-ari-dev.js
@@ -25,6 +25,21 @@ const CustomOpenURLExtension = {
     }
 };
 
+
+  (function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '67c42da6a9ca2ac532c2c721' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production'
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+
+
+/*
 (function(d, t) {
     var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
     v.onload = function() {
@@ -36,11 +51,11 @@ const CustomOpenURLExtension = {
                 extensions: [CustomOpenURLExtension],
                 stylesheet:'https://ea-ari-demo1.vercel.app/voiceflow.fr.css'
              }
-             /*,
+             
             voice: { 
                 url: "https://runtime-api.voiceflow.com" 
             }
-                */
+                
         });
     };
     //v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
@@ -48,3 +63,4 @@ const CustomOpenURLExtension = {
     v.type = "text/javascript";
     s.parentNode.insertBefore(v, s);
 })(document, 'script');
+*/
